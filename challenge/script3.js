000bbfc05b24554b5e0f5dbe5dbacb22ab4450b9
Muhammad Usama <muhammadusama30289@gmail.com>
2023-02-28 22:56:33 +0500

    initial

---
+++ challenge/script3.js
@@ -0,0 +1,18 @@
+(function () {
+  "use strict";
+
+  const pTag = document.querySelector("p");
+  const btn = document.querySelector("button");
+
+  btn.addEventListener("click", function () {
+    pTag.style.color = "green";
+  });
+})();
+
+// Practice
+
+(function () {
+  "use strict";
+  const myName = "Usama";
+  const myFatherName = "A Qadeer";
+})();
