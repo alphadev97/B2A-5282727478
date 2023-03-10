43d7930d2a987713108cbb7e02c03ce87add6b8f
Muhammad Usama <muhammadusama30289@gmail.com>
2023-03-10 23:59:49 +0500

    initial

---
+++ JS and JQuery/1.3-Tabbed-interface-JS/script.js
@@ -0,0 +1,21 @@
+var tabs = document.querySelectorAll("#tabs > ul > li > a");
+
+for (var i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", selectTab);
+}
+
+function selectTab(event) {
+  event.preventDefault();
+
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].removeAttribute("class");
+  }
+
+  event.target.className = "active";
+
+  var thisTab = event.target.getAttribute("href");
+  var thisContent = document.querySelector(thisTab);
+  var oldContent = document.querySelector(".visible");
+
+  oldContent.className = "visuallyhidden";
+}
