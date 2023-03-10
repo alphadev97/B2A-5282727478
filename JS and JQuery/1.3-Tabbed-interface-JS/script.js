c56d1f2e96e6c16cad42e2997603650e149f4884
Muhammad Usama <muhammadusama30289@gmail.com>
2023-03-11 01:26:29 +0500

    Update script.js

--- JS and JQuery/1.3-Tabbed-interface-JS/script.js
+++ JS and JQuery/1.3-Tabbed-interface-JS/script.js
@@ -1,21 +1,36 @@
-var tabs = document.querySelectorAll("#tabs > ul > li > a");
+(function () {
+  "use strict";
 
-for (var i = 0; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", selectTab);
-}
+  const tabs = document.querySelectorAll("#tabs > ul > li > a");
 
-function selectTab(event) {
-  event.preventDefault();
-
-  for (var i = 0; i < tabs.length; i++) {
-    tabs[i].removeAttribute("class");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", selectTab);
   }
 
-  event.target.className = "active";
+  function selectTab(event) {
+    event.preventDefault();
 
-  var thisTab = event.target.getAttribute("href");
-  var thisContent = document.querySelector(thisTab);
-  var oldContent = document.querySelector(".visible");
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].removeAttribute("class");
+    }
 
-  oldContent.className = "visuallyhidden";
-}
+    event.target.className = "active";
+
+    const thisTab = event.target.getAttribute("href");
+    const thisContent = document.querySelector(thisTab);
+    const oldContent = document.querySelector(".visible");
+
+    oldContent.className = "visuallyhidden";
+    oldContent.addEventListener(
+      "transitionend",
+      function () {
+        oldContent.className = "hidden";
+        thisContent.className = "visible visuallyhidden";
+        setTimeout(function () {
+          thisContent.classList.remove("visuallyhidden");
+        }, 20);
+      },
+      { capture: false, once: true, passive: false }
+    );
+  }
+})();
