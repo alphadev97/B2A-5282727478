51e6351d899f3e9275389a967c363448c89f85ba
Muhammad Usama <muhammadusama30289@gmail.com>
2023-03-14 16:33:49 +0500

    initial

--- JS and JQuery/1.6-Basic-JS-Img-Slider/script.js
+++ JS and JQuery/1.6-Basic-JS-Img-Slider/script.js
@@ -1,14 +1,34 @@
 window.addEventListener("load", function () {
-  const slideCount = document.querySelectorAll("#slider-wrapper ul li").length;
-  //   alert(slideCount);
+  const slideCount = document.querySelectorAll("#slider-wrapper ul li").length; //   How many slides?
 
-  const slideWidth = document.querySelector("#slider-wrapper").offsetWidth;
+  const slideWidth = document.querySelector("#slider-wrapper").offsetWidth; // How wide is each slide?
 
-  const totalWidth = slideCount * slideWidth + "px";
+  const totalWidth = slideCount * slideWidth + "px"; // Total slider width
+
+  // DOM Elements
 
   const slider = document.querySelector("#slider-wrapper ul");
 
   const next = document.querySelector("#next");
 
   const previous = document.querySelector("#prev");
+
+  let leftPosition = 0;
+
+  let counter = 0;
+
+  slider.style.width = totalWidth;
+
+  next.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    counter++;
+
+    if (counter === slideCount) {
+      counter = 0;
+      leftPosition = 0;
+      slider.style.left = leftPosition;
+    } else {
+    }
+  });
 });
