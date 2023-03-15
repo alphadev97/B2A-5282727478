d7354a0d6e7a416e1f10f704651b9342177ee068
Muhammad Usama <muhammadusama30289@gmail.com>
2023-03-16 00:47:14 +0500

    initial

--- 04. jQuery Plugins/1.3-Smooth-Scroll-Effect-JQ/smoothscroll.js
+++ 04. jQuery Plugins/1.3-Smooth-Scroll-Effect-JQ/smoothscroll.js
@@ -1,2 +1,20 @@
-// Smoothscroll Script...
+$("nav ul li a").click(function () {
+  const thisSection = $(this).attr("href");
+  let thisLink = $(this);
 
+  $("html, body")
+    .stop()
+    .animate(
+      {
+        scrollTop: $(thisSection).offset().top - 200,
+      },
+      400,
+      "easeOutCirc",
+      function () {
+        $("nav ul li a").removeAttr("class");
+        thisLink.addClass("selected");
+
+        // alert($(window).scrollTop());
+      }
+    );
+});
