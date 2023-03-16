15643da629f600fc5528554b32bb4fc5a14a8391
Muhammad Usama <muhammadusama30289@gmail.com>
2023-03-16 22:48:51 +0500

    Update smoothscroll.js

--- 04. jQuery Plugins/1.4-Smooth-Scrolling-Effect-JQ/smoothscroll.js
+++ 04. jQuery Plugins/1.4-Smooth-Scrolling-Effect-JQ/smoothscroll.js
@@ -20,15 +20,16 @@ $("nav ul li a").click(function () {
 });
 
 $(window).on("load", function () {
+  var allLinks = $("nav ul li a");
   var posts = $("section");
   var pageTop;
   var postPos;
   var counter = 0;
+  var prevCounter = 0;
+  var doneResizing;
   var postTop = [];
 
-  posts.each(function () {
-    postTop.push(Math.floor($(this).offset().top));
-  });
+  resetPagePosition();
 
   console.log(postTop);
 
@@ -37,7 +38,46 @@ $(window).on("load", function () {
 
     if (pageTop > postTop[counter + 1]) {
       counter++;
-      console.log(`scrolling down ${counter}`);
+      // console.log(`scrolling down ${counter}`);
+    } else if (counter > 0 && pageTop < postTop[counter]) {
+      counter--;
+      // console.log(`scrolling up! ${counter}`);
+    }
+    if (counter != prevCounter) {
+      $(allLinks).removeAttr("class");
+      $("nav ul li a").eq(counter).addClass("selected");
+      prevCounter = counter;
     }
   });
+
+  // Capturing resizing windows
+
+  $(window).on("resize", function () {
+    clearTimeout(doneResizing);
+    doneResizing = setTimeout(function () {
+      resetPagePosition();
+    }, 500);
+  });
+
+  function resetPagePosition() {
+    postTop = [];
+
+    posts.each(function () {
+      postTop.push(Math.floor($(this).offset().top));
+    });
+
+    var pagePosition = $(window).scrollTop() + 210;
+    counter = 0;
+
+    for (var i = 0; i < postTop.length; i++) {
+      if (pagePosition > postTop[i]) {
+        counter++;
+      }
+    }
+
+    counter--;
+
+    $(allLinks).removeAttr("class");
+    $("nav ul li a").eq(counter).addClass("selected");
+  }
 });
